9e103a625f2ede261458f5fb798656c0db3b45e3
Annika dL <89029618+twrdx@users.noreply.github.com>
2023-02-06 17:02:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function preload() {
+  tausta = loadImage('game-bg.png');
+	donut = loadImage('donut3.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowWidth / 3);
+}
+
+function draw() {
+  background(220);
+}
+
